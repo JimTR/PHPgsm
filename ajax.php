fabaa900377ec01d8af0c15073f50fee72fef9f3
JimTR <jim.richardson06@btinternet.com>
2019-12-28 16:11:24 +0000

    beta ajax server

---
+++ ajax.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * ajax.php
+ * 
+ * Copyright 2019 Jim Richardson <jim@noideersoftware.co.uk>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * required for ajax requests from html version 
+ */
+ 
+ require 'includes/cli_master.inc.php';
+ include 'functions.php';
+ define ("CR","<br>");
+ if(isset($_GET['action'])) {
+	// echo $_GET['action']; //debug action
+	switch (strtolower($_GET['action'])) {
+		case "boottime" :
+			echo get_boot_time();
+			exit;
+			
+		case "load" :
+			$cpu_info=get_cpu_info();
+			echo $cpu_info['load'];
+			//print_r ($cpu_info['load']);
+			exit;
+		case "rgames" :
+		  echo  display_games();
+		  exit;	
+		}
+}
+else {
+	echo "you cocked up";
+}
+?>
