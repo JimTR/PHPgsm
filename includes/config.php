df5aaeb64a3806e21b1c77302a88bec241b9eb75
Jim <jim@noideersoftware.co.uk>
2020-02-23 03:33:19 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -7,10 +7,10 @@ $config['database']['type'] = 'mysqli';
 $config['database']['database'] = 'gsm';
 $config['database']['default_table_prefix'] = 'gsm_'; // for shared database
 $config['database']['hostname'] = 'localhost';
-$config['database']['username'] = 'nod';
-$config['database']['password'] = 'ahamay900';
+$config['database']['username'] = 'username';
+$config['database']['password'] = 'password';
 $config['database']['encoding'] = 'utf8';
-$config['database']['errors'] = "jim@noideersoftware.co.uk";
+$config['database']['errors'] = "email address";
 $config['database']['useDBSessions'] =  "1";
 $config['database']['display_error'] = "1";
 ?>
