72f6a671bb90eab2b659af3aed652acf2dcd7c91
JimTR <jim.richardson06@btinternet.com>
2020-02-23 03:28:52 +0000

    Start of GB Language File

---
+++ lang/english_GB.php
@@ -0,0 +1,13 @@
+<?php
+$l['game_servers'] = 'Game Servers';
+$l['host_servers'] = 'Host Servers';
+$l['online_games'] = 'Online Game Servers';
+$l['started'] = 'Started at';
+$l['players'] = 'Players Online';
+$l['map'] = 'Map';
+$l['on'] = 'on';
+$l['uptime'] = 'Up Time';
+$l['cpu_model'] = 'CPU Model';
+$l['cpu_processors'] = 'Cpu Processors';
+$l['cpu_cores'] = 'Cpu Cores';
+?>
