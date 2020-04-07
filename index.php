39196b1cd23736ccd1ee706a6554852ef651ab78
JimTR <jim@noideersoftware.co.uk>
2020-04-07 15:41:11 +0100

    removed obsolete code

--- index.php
+++ index.php
@@ -6,9 +6,6 @@ require 'includes/master.inc.php'; // do login and stuff
 define ("CR", "</br>");
 
 $sql = 'select * from base_servers where extraip="0" and enabled="1"';
-
-
-
 	
 	$database = new db(); // connect to database
 	
@@ -43,27 +40,7 @@ foreach ($res as $data) {
 	//add the data array for base server 
 	//this does allow remote locations
 	// as long as you have the remote software installed
-	
-	/*$subpage['server_title'] = $data['name'].' ('.$data['ip'].')';
-	$subpage['host'] = $data['url'].':'.$data['port'] ;
-	$subpage['id'] = 'collapse'.$acc ;
-	$subpage['ip'] = $data['ip'];
-	// curl the data for the server
-		$ch = curl_init();
-	     curl_setopt($ch, CURLOPT_URL, $subpage['host'].'/ajax.php?action=all');
-	     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		 $server = curl_exec($ch);
-		 curl_close($ch);
-	     $server= explode('\n',$server);
-	
-	$subpage['cpu'] = $server[0];
-	$subpage['software'] = $server[1];
-	$subpage['disk'] = $server[2];
-	$subpage['mem'] = $server[3];
-	$subpage['user'] = $server[4];
-	$template->replace_vars($subpage);
-	$page1.= $template->get_template();
-	*/  
+		
 	}
 	
 		
@@ -75,13 +52,19 @@ foreach ($res as $data) {
 		// loop servers
 		
 		$template->load('html/game_server.html'); // load blank template
-		$action ='dt';
 		$server = $data['location'].'/'.$data['host_name'];
 		$url = $data['url'].':'.$data['bport'];
 		$servers['cmd'] = $data['startcmd'];
+		 $ch = curl_init();
+	     curl_setopt($ch, CURLOPT_URL, $url.'/ajax.php?action=user');
+	     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		 $user = curl_exec($ch);
+		 curl_close($ch);
+		 
 		 // make gameq call
 		 //print_r($servers);
 		 //echo CR;
+		 //xml remove starts here
 		 require_once('GameQ/Autoloader.php'); //load GameQ
 		 $key = $data['host_name'];
 		 $x2['id'] = $data['host_name'];
@@ -89,12 +72,7 @@ foreach ($res as $data) {
 	     $x2['type'] = $data['type'];
 	     //print_r($x2);
 	     //echo CR;
-	     $ch = curl_init();
-	     curl_setopt($ch, CURLOPT_URL, $url.'/ajax.php?action=user');
-	     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		 $user = curl_exec($ch);
-		 curl_close($ch);
-		 
+
 	     $GameQ = new \GameQ\GameQ();
 		//include ("server-info.php");
           $GameQ->addServer($x2);
@@ -105,7 +83,7 @@ foreach ($res as $data) {
 		  $logo = $data['logo'];
 		
 		 if ($results[$key]['gq_online'] == 1 ){
-			  //online
+			  //online xml needs to do this
 			  $online = 'img/online.png';
 			  $subpage['sbstate']='disabled';
 			  $subpage['ststate']='';
@@ -116,6 +94,7 @@ foreach ($res as $data) {
 			  $subpage['bstate']='disabled';
 		  }
 		  else {$online = 'img/offline.png';
+			  // xml needs to do this 
 			    $subpage['sbstate']='';
 			    $subpage['ststate']='disabled';
 			    $subpage['scstate']='disabled';
@@ -129,17 +108,7 @@ foreach ($res as $data) {
 		 $subpage['sourceport'] = $data['source_port'];
 		 $subpage['clientport'] = $data['client_port'];	
 		 $subpage['server_name'] = $key.' ('.$data['host'].':'.$data['port'].')';
-		 $subpage['lgsm'] = substr($servers['LinuxGSM version'],1);
 		 $subpage['cmd'] = $servers['cmd'];
-		 $subpage['Discord alert'] = $servers['Discord alert'];
-		 $subpage['Slack alert'] = $servers['Slack alert'];
-		 $subpage['Email alert'] = $servers['Email alert'];
-		 $subpage['Update on start'] = $servers['Update on start'];
-		 $subpage['Pushbullet alert'] = $servers['Pushbullet alert'];
-		 $subpage['IFTTT alert'] = $servers['IFTTT alert'];
-		 $subpage['Mailgun (email) alert'] = $servers['Mailgun (email) alert'];
-		 $subpage['Pushover alert'] = $servers['Pushover alert'];
-		 $subpage['Telegram alert'] = $servers['Telegram alert'];
 		 $subpage['players'] = $results[$key]['gq_numplayers'];
 		 $subpage['Current map'] = $results[$key]['gq_mapname'];
 		 $subpage['Maxplayers'] = $data['max_players'];
@@ -153,6 +122,7 @@ foreach ($res as $data) {
 		 $subpage['key'] = $key;
 		 $subpage['url'] = $url;
 		 $subpage['logo']  = $logo;
+		 // xml finish
 		 $template->replace_vars($subpage);
 		 $page2.= $template->get_template(); 
         // echo $page2.CR;
