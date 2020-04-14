59b668e585f256cc18dc9404c1fa0815d4d58c06
JimTR <jim@noideersoftware.co.uk>
2020-04-14 12:00:00 +0100

    change html code

--- functions.php
+++ functions.php
@@ -847,7 +847,7 @@ function html_display($tm,$results) {
 					// end of players for each
 					$disp .='</tbody></table><br></div><script>
 		$("#gol'.$key.'").click(function(){
-		$("#ops'.$key.").slideToggle("slow");
+		$("#ops'.$key.'").slideToggle("slow");
   });
  	</script>';
 				}
