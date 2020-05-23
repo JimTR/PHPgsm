af2367ab6850b7ed17d6f40c4afda62d20e65855
JimTR <jim@noideersoftware.co.uk>
2020-05-23 11:01:58 +0100

    Server Version

--- ajax.php
+++ ajax.php
@@ -474,6 +474,7 @@ function check_update()
 		if (!empty($files)){
 			echo $files[0];// get file 
 			$acf_file = get_file_contents($files[0]);
+			echo $acf_file.'<br>';
 			$local =  local_build($acf_file);
 			echo 'Local Build id '.$local['buildid'];
             echo 'Last Local Update '.date('l jS F Y \a\t g:ia',$local['update']).PHP_EOL;
