ee9450c51afc5f84fcc84071823083fd69c3f350
JimTR <jim@noideersoftware.co.uk>
2020-07-29 11:16:11 +0100

    testing build id's

--- cron_u.php
+++ cron_u.php
@@ -46,7 +46,7 @@ $sql = 'SELECT servers.* , base_servers.url, base_servers.port FROM `servers` le
 			}
 		}
 		else {
-			$local['appid'] = $data['app_id'];
+			$local['appid'] = $data['server_id'];
 			$local['buildid'] = $data['buildid'];
 			$local['update'] = $data['server_update'];
 			
