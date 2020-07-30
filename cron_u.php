5593bbd4228990a29471c34fc4dcd75beea2ff98
JimTR <jim@noideersoftware.co.uk>
2020-07-30 09:22:23 +0100

    cron update

--- cron_u.php
+++ cron_u.php
@@ -91,9 +91,11 @@ $sql = 'SELECT servers.* , base_servers.url, base_servers.port FROM `servers` le
 		function local_update($build,$local) {
 			
 			//
-			echo $build['bulidid'].cr;
 			$acf_loc = $build['location'].'/serverfiles/steamapps';
 			echo $acf_loc.cr;
-			
+			$find = 'appmanifest_';
+		    $files = glob($acf_loc."/*" . $find . "*");
+			$acf_file = file_get_contents($files[0]);
+			echo $files[0];
 		}	
 ?>
