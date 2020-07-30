f43ddbae16b998688b7c8debbb8afc54c9231c41
JimTR <jim@noideersoftware.co.uk>
2020-07-30 09:56:32 +0100

    cron update

--- cron_u.php
+++ cron_u.php
@@ -66,7 +66,7 @@ $sql = 'SELECT servers.* , base_servers.url, base_servers.port FROM `servers` le
 				$man_check = local_update($data,$local); // check if manual update has been done
 				if($man_check <> $local['buildid']) {
 					$local['buildid'] = $man_check;
-					unset($data['buildid']);
+					$data['buildid']=0;
 					echo 'Correcting Build'.cr;
 				}
 			    $update['server_id'] = $local['appid'];;
@@ -77,6 +77,7 @@ $sql = 'SELECT servers.* , base_servers.url, base_servers.port FROM `servers` le
 			    $where['server_id'] = $local['appid']; // update all servers with that app with the current build 
 			    if ($data['rbuildid'] <> $remote['buildid']) {
 					// just update if there is an updated build
+					echo 'update '.cr;
 					$database->update('servers',$update,$where);
 				}
 			    echo 'Details for App Id '.$local['appid'].cr;
