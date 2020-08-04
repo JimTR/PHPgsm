fa0e84441e3e3e12ec6df3664e91df3301f33096
JimTR <jim@noideersoftware.co.uk>
2020-08-04 13:34:04 +0100

    auto user update on log in

--- readlog.php
+++ readlog.php
@@ -12,6 +12,10 @@ $data['content'] = $_GET['time'] < $data['time']
     : false;
 
 foreach ($data['content'] as $k => $v ) {
+	$x = strpos($v,'" connected, address "');
+	if ($x >0 ) {
+		echo 'new  ';
+	}
 	$v = preg_replace('/Console<0><Console><Console>/','Console',$v);
 	$v = preg_replace('/<[U:1:[0-9]+]>/', ' ', $v);
 	$v = preg_replace('/</',' ',$v);
