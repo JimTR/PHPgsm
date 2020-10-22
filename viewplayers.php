5b9a1f8d52fdef5f9bc7f2661787caed4c55289b
JimTR <jim@noideersoftware.co.uk>
2020-10-22 10:05:17 +0100

    altered table

--- viewplayers.php
+++ viewplayers.php
@@ -53,7 +53,7 @@ $players = 	$results[$key]['gq_numplayers'];
 if ($players >0) {
 					// we have players
 					// add sub template
-					$disp .= '<table style="width:100%;"><tr><td style="width:35%;">Name</td><td style="width:30%;">Country</td><td style="width:15%;">Score</td><td>Time Online</td></tr>'; // start table
+					$disp .= '<table style="width:100%;"><tr><td style="width:40%;">Name</td><td style="width:30%;">Country</td><td style="width:10%;">Score</td><td>Time Online</td></tr>'; // start table
 					$player_list = $results[$key]['players']; // get the player array
 					orderBy($player_list,'gq_score');
 					foreach ($player_list as $k=>$v) {
