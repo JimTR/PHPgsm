656ab0551b8c21a021384b5ec010b4705487899a
JimTR <jim@noideersoftware.co.uk>
2020-11-24 12:03:58 +0000

    test filtered gs data

--- ajax.php
+++ ajax.php
@@ -566,7 +566,11 @@ function game_detail() {
 					exit;
 					// kill if wrong
 				}
-                //$new = trim(file_get_contents($server_data['url'].':'.$server_data['bport'].'/ajax.php?action=ps_file&filter='.$server_data['host_name']));
+                $new = trim(file_get_contents($server_data['url'].':'.$server_data['bport'].'/ajax.php?action=ps_file&filter='.$server_data['host_name']));
+                if (empty($new)) {
+		// offline
+		echo 'offline<br>';
+	}
                 print_r($server_data);
                 //echo $new;
                 $tmp = explode(' ',$new);
