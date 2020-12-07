e75fa278a985831b905c13e16d0542a2b6cf9ebd
JimTR <jim@noideersoftware.co.uk>
2020-12-07 12:47:30 +0000

    test all option

--- ajax.php
+++ ajax.php
@@ -675,6 +675,7 @@ function game_detail() {
 		$cmd = 'top -b -n 1 -p '.$pid.' | sed 1,7d'; // use top to query the process
 		$top = array_values(array_filter(explode(' ',trim(shell_exec($cmd))))); // arrayify
 		$sql = 'select * from servers where servers.host ="'.$tmp_array[$i][6].'" and servers.port = "'.$tmp_array[$i][8].'"'; //query 1 get the server detail
+		echo $sql.cr;
 		$result = reset($db->get_results($sql)); // get data back
 		$sql = 'select  count(*) as total from servers where servers.host like "'.substr($tmp_array[$i][6],0,strlen($tmp_array[$i][6])-1).'%"'; // query 2 count the game servers
 		$server_count= reset($db->get_results($sql)); // get data back
