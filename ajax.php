26385c9818acb51c035a511d26b2d048472f75b5
JimTR <jim@noideersoftware.co.uk>
2021-01-07 06:03:16 +0000

    change location

--- ajax.php
+++ ajax.php
@@ -645,7 +645,7 @@ function game_detail() {
 		$ip = file_get_contents("http://ipecho.net/plain"); // get ip
 		 if (empty($ip)) { $ip = shell_exec('curl http://ipecho.net/plain');} 
 		 $sql = 'select servers.* , base_servers.port as bport, base_servers.base_ip, base_servers.url from servers left join base_servers on servers.host = base_servers.ip where servers.host_name = "'.$cmds['filter'].'"';
-		 echo $sql.'<br>';		 
+		 //echo $sql.'<br>';		 
 		 $server_data = $db->get_results($sql);
 		  $server_data=reset($server_data);
 		  if (empty($server_data['base_ip'])) {         
@@ -679,7 +679,7 @@ function game_detail() {
 	
 	$pid = $tmp[0];
 	$count = count($tmp);
-	echo 'using command '.$server_data['url'].':'.$server_data['bport'].'/ajax.php?action=top&filter='.$pid.'&key='.md5( ip2long($ip)).'<br>';
+	//echo 'using command '.$server_data['url'].':'.$server_data['bport'].'/ajax.php?action=top&filter='.$pid.'&key='.md5( ip2long($ip)).'<br>';
 	$temp =  trim(file_get_contents($server_data['url'].':'.$server_data['bport'].'/ajax.php?action=top&filter='.$pid.'&key='.md5( ip2long($ip))));
         //$temp = trim(file_get_contents('top');
 	$temp = array_values(array_filter(explode(' ',$temp)));
