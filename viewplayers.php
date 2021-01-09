61c2cc2974d162e595da1d32c12293bb12fb1cf3
JimTR <jim.richardson06@btinternet.com>
2021-01-09 10:16:48 +0000

    converted to Xpaw

--- viewplayers.php
+++ viewplayers.php
@@ -23,14 +23,20 @@
  * remember js
  */
 
-
-$servers = [
-    
-    'id'      => $_GET['id'],
-    'type'    => $_GET['type'],
-    'host'    => $_GET['host']
-    
-];
+require __DIR__ . '/xpaw/SourceQuery/bootstrap.php';
+use xPaw\SourceQuery\SourceQuery;
+$x = strpos($_GET['host'],':');
+$sport = substr($_GET['host'],$x+1);
+$ip = substr($_GET['host'],0,$x);
+define( 'SQ_SERVER_ADDR', $ip );
+	define( 'SQ_SERVER_PORT', $sport );
+	define( 'SQ_TIMEOUT',     1 );
+	define( 'SQ_ENGINE',      SourceQuery::SOURCE );
+$Query = new SourceQuery( );
+$Query->Connect( SQ_SERVER_ADDR, SQ_SERVER_PORT, SQ_TIMEOUT, SQ_ENGINE );
+$results = $Query->GetPlayers( ) ;
+$info = $Query->GetInfo();
+$Query->Disconnect( );
  //header('Access-Control-Allow-Origin: *');
 //46.32.237.232:27016
 require ('includes/master.inc.php');
@@ -45,29 +51,29 @@ $browser = get_browser(null, true);
 if (empty($_GET['id'])) {
 	redirect('/');
 }
-require_once('GameQ/Autoloader.php');
+//require_once('GameQ/Autoloader.php');
 require 'includes/Emoji.php';
 $sql = 'select * from players where BINARY name="';
-$GameQ = new \GameQ\GameQ();
+//$GameQ = new \GameQ\GameQ();
 //echo "new gameq<br>";
-$GameQ->addServer($servers);
+//$GameQ->addServer($servers);
 //echo "added server<br>";
-$results = $GameQ->process();
+//$results = $GameQ->process();
 //echo "good to here<br>";
-//print_r($results);
-$key = $servers['id'];
-$disp .='<div style= "text-align:center;" ><span class="c_map">Current Map </span>: &nbsp;<span class="c_map_n">'.$results[$key]['gq_mapname'].'</span>&nbsp;&nbsp;<span class="pol"> Players Online</span>&nbsp;<span class="numplayers">'.$results[$key]['gq_numplayers'].'</span>/<span class ="maxplayers">'.$results[$key]['gq_maxplayers'].'</span> </div>';
-$players = 	$results[$key]['gq_numplayers'];
-if ($players >0) {
+//print_r($info);
+//$key = $servers['id'];
+$disp .='<div style= "text-align:center;" ><span class="c_map">Current Map </span>: &nbsp;<span class="c_map_n">'.$info['Map'].'</span>&nbsp;&nbsp;<span class="pol"> Players Online</span>&nbsp;<span class="numplayers">'.$info['Players'].'</span>/<span class ="maxplayers">'.$info['MaxPlayers'].'</span> </div>';
+
+if ($info['Players'] >0) {
 					// we have players
 					// add sub template
 					$disp .= '<table style="width:100%;border-collapse: inherit;border-spacing: 0px .4em;"><tr class="country"><td style="width:40%;">Name</td><td style="width:30%;">Country</td><td style="width:10%;">Score</td><td>Time Online</td></tr>'; // start table
-					$player_list = $results[$key]['players']; // get the player array
-					orderBy($player_list,'gq_score');
+					$player_list = $results; // get the player array
+					orderBy($player_list,'Frags');
 					foreach ($player_list as $k=>$v) {
 						//loop through player array
 						//$playerN = substr($player_list[$k]['gq_name'],0,20); // chop to 20 chrs
-						$playerN = $player_list[$k]['gq_name'];
+						$playerN = $player_list[$k]['Name'];
 						
 						
 						$playerN2 = Emoji::Encode($playerN);
@@ -82,23 +88,26 @@ if ($players >0) {
 						$playerN = str_pad($playerN,25); //pad to 25 chrs
 						switch (true) {
 							// format score
-							case  ($player_list[$k]['gq_score']<0) :
+							case  ($player_list[$k]['Frags']<0) :
 								// minus
-								$pscore = '&nbsp;&nbsp;&nbsp;'.$player_list[$k]['gq_score']; //format score
+								$pscore = '&nbsp;&nbsp;&nbsp;'.$player_list[$k]['Frags']; //format score
 								break;
-							case  ($player_list[$k]['gq_score']<10) :
+							case  ($player_list[$k]['Frags']<10) :
 								//
-								$pscore = '&nbsp;&nbsp;&nbsp;&nbsp;'.$player_list[$k]['gq_score']; //format score
+								$pscore = '&nbsp;&nbsp;&nbsp;&nbsp;'.$player_list[$k]['Frags']; //format score
 								break;
-								case  ($player_list[$k]['gq_score']<100) :
+								case  ($player_list[$k]['Frags']<100) :
 								//
-								$pscore = '&nbsp;&nbsp;'.$player_list[$k]['gq_score']; //format score
+								$pscore = '&nbsp;&nbsp;'.$player_list[$k]['Frags']; //format score
 								break;
-							case  ($player_list[$k]['gq_score']<1000)	:
+							case  ($player_list[$k]['Frags']<1000)	:
 								//
-								$pscore = $player_list[$k]['gq_score']; //format score
+								$pscore = '&nbsp;&nbsp;'.$player_list[$k]['Frags']; //format score
 								break;
+							 default:
+                                                                $pscore = $player_list[$k]['Frags'];
 						}
+
 						// format display here
 						// add sub template
 							if (!empty($result)) {
@@ -113,11 +122,11 @@ if ($players >0) {
 							}			
 							
 							//$map = Emoji::Decode($result['flag']); //get flag
-							$disp .='<tr class="country"><td><i class="player_n" style="">'.$playerN.'</i></td><td>'.$map.' <span class="country">'.$result['country'].'</span></td><td>'.$pscore.'</td><td style="padding-left:1%;">'.gmdate("H:i:s", $player_list[$k]['gq_time']).'</td></tr>';
+							$disp .='<tr class="country"><td><i class="player_n" style="">'.$playerN.'</i></td><td>'.$map.' <span class="country">'.$result['country'].'</span></td><td>'.$pscore.'</td><td style="padding-left:1%;">'. $player_list[$k]['TimeF'].'</td></tr>';
 							//print_r($result);
 						}
 						else {
-									$disp .='<tr class="country"><td><i class="player_n" style="" >'.$playerN.'</i></td><td></td><td>'.$pscore.'</td><td style="padding-left:1%;">'.gmdate("H:i:s", $player_list[$k]['gq_time']).'</td></tr>';
+									$disp .='<tr class="country"><td><i class="player_n" style="" >'.$playerN.'</i></td><td></td><td>'.$pscore.'</td><td style="padding-left:1%;">'. $player_list[$k]['TimeF'].'</td></tr>';
 						}
 						
 						
