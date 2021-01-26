cde85567862a44e873c5f14cc280ec292145b416
JimTR <jim@noideersoftware.co.uk>
2021-01-26 17:26:57 +0000

    test

--- functions.php
+++ functions.php
@@ -242,7 +242,7 @@ function get_user_info ($Disk_info) {
 		}
 	    else 
 	    {
-			$user['quota_free'] = dataSize(intval($user['quota'])*1000 -intval($user['quota_used'])*1000);
+			$user['quota_free'] = dataSize(intval($user['quota'])*100000 -intval($user['quota_used'])*100000);
 			}
 	}
 	//print_r($user);
