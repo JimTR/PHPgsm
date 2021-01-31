9da85763bdbd2d5c3d33b7ecbcbc20929cf23938
Jim <jim@noideersoftware.co.uk>
2021-01-31 08:38:53 +0000

    Removed remarks
    Go back to this after ajaxv2

--- utils/cron_r.php
+++ utils/cron_r.php
@@ -38,50 +38,36 @@ define( 'SQ_ENGINE',      SourceQuery::SOURCE );
 $sql = 'select * from servers where running=1';
 $sql = 'SELECT servers.* , base_servers.url, base_servers.port as bport, base_servers.fname,base_servers.ip as ipaddr, base_servers.base_ip,base_servers.password FROM `servers` left join `base_servers` on servers.host = base_servers.ip where servers.id <>"" and servers.running="1" order by servers.host_name';
 $games = $database->get_results($sql);
-//print_r($games);
+
 foreach ($games as $game) {
 		if (ping($game['host'], $game['port'], 1)) {
-			//echo 'ok'.cr;
-		
 		$Query->Connect( $game['host'], $game['port'], 1, SQ_ENGINE );
 		$info = $Query->GetInfo();
 		$Query->Disconnect( );
-		//echo print_r($info,true).cr;
 		if ($info['Players'] == 0 ) {
-			//echo $info['HostName']. ' can be restarted  '.cr;
-			//echo 'use '.$game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr']).cr;
 			$game['restart']=  $game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr']);
 			$restart[] = $game;
 			 $game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr']);
-			//echo file_get_contents($game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr'])).cr;
 		}
 		elseif ($info['Bots'] == $info['Players']) {
-			//echo $info['HostName']. ' can be restarted [bots] '.cr;
-			//echo 'use '.$game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr']).cr;
 			$game['restart'] = $game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr']);
 			$restart[] = $game;
-			//echo file_get_contents($game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr'])).cr;
 		}
 		else  {
-			//echo $info['HostName']. ' can not be restarted ('.$info['Players'].' players online)'.cr;
 			$game['restart'] = $game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr']);
 			$check[] = $game; 
 		}
 	}
 	else { continue;}
 }
-	//print_r ($restart);
 	echo 'Restarting '.count($restart).'/'.count($games).' server(s)'.cr;
 	foreach ($restart as $game) {
-		echo 'restarting '.$game['server_name'].' - ';
-		//print_r($game);
-		echo file_get_contents($game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr'])).cr;
-		sleep(1);
-		//echo 'restarted '.cr;
-	}
+			echo file_get_contents($game['url'].':'.$game['bport'].'/ajax.php?action=exescreen&cmd=r&exe='.$game['host_name'].'&key='.md5($game['ipaddr'])).cr;
+			sleep(1);
+			}
 	
 	
-	//print_r($check);
+	
 	if (isset($check)) { 
 		echo 'Defered '.count($check).'/'.count($games).' server(s)'.cr;
 	foreach ($check as $restart) {
