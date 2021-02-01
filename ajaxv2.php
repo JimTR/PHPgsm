cb2b1fd790dfb0c5d9b803b2d1c1ddf8616727b1
Jim <jim@noideersoftware.co.uk>
2021-02-01 07:39:56 +0000

    another thought

--- ajaxv2.php
+++ ajaxv2.php
@@ -382,7 +382,7 @@ function all($cmds) {
 			$return = array_merge($return,get_disk_info());
 			$return = array_merge($return,get_mem_info());
 			$return = array_merge($return,get_user_info($return));
-			$return = array_merge($return,game_detail());
+			$return['servers'] = game_detail();
 			return $return;
 		}	
 function exescreen ($cmds) {
