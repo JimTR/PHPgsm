9597f246cdc6a09581a7c7ea50234a188991ba03
Jim <jim@noideersoftware.co.uk>
2021-04-09 13:48:01 +0100

    test file

--- install/install.php
+++ install/install.php
@@ -7,12 +7,16 @@ if (!defined('PHP_VERSION_ID')) {
     $version = explode('.', PHP_VERSION);
     define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
 }
- define('PHP_MAJOR_VERSION',   $version[0]);
+    define('PHP_MAJOR_VERSION',   $version[0]);
     define('PHP_MINOR_VERSION',   $version[1]);
     define('PHP_RELEASE_VERSION', $version[2]);
 $cc = new Console_Color2();
 $tick = $cc->convert("%g  ✔%n");
 $cross = $cc->convert("%r  ✖%n");
+$req = $cc->convert("%gRequired%n");
+$rreq = $cc->convert("%rRequired%n");
+$opt = $cc->convert("%gOptional%n");
+$ropt = $cc->convert("%rOptional%n");
  $table = new Console_Table(
     CONSOLE_TABLE_ALIGN_LEFT,
     array('horizontal' => '', 'vertical' => '', 'intersection' => '')
@@ -23,6 +27,9 @@ system('clear');
 echo $cc->convert("%cPHPgsm Installer%n").cr; 
 //echo get_boot_time().' '.$tick.cr;
 $x32 = trim(shell_exec('dpkg --print-foreign-architectures'));
+if (empty($x32)) {
+	$x32 = 'Not Enabled';
+}
 $table->addRow(array('Module','   Version' ,'Status','Usage'));
 
 $loc =dpkg('mlocate');
@@ -32,76 +39,77 @@ $steam = dpkg('steamcmd:i386');
 $glib = dpkg('libc-bin');
 $webmin = dpkg('webmin');
 $st = dpkg('mysql-server');
+if(isset($steam[2])){
+	$software['Steamcmd']['version']  = $steam[2];
+	$software['Steamcmd']['use']  = $req.' - '.$steam[4].' install & update Steam dedicated game servers';
+}
+
+else {
+	$software['Steamcmd']['version']  = $steam[1];
+	$software['Steamcmd']['use']  = $rreq.' -  use :- install & update Steam dedicated game servers';
+}
+if (isset($glib[2])){
+	$software['GlibC']['version'] = $glib[2];
+	$software['GlibC']['use'] = $req.' - '.$glib[4].' for Steam dedicated game servers';
+}
+else {
+	$software['GlibC']['version'] = $glib[1];
+	$software['GlibC']['use'] = $rreq.' -  for Steam dedicated game servers';
+}	
+$software['foreign_architecture']['version'] = $x32;
+$software['foreign_architecture']['use'] = $req.' - Steamcmd requires 32bit architecture';
 if (!isset($st[2])) {
 	$st = dpkg('mysql-common');
 }
 if (isset($st[2])) {
 	$software['Mysql']['version'] = $st[2];
-	$software['Mysql']['use'] = 'Optional - '.$st[4];
+	$software['Mysql']['use'] = $opt.' - '.$st[4];
 }
 else {
 	$software['Mysql']['version'] = $st[1];
-	$software['Mysql']['use'] = 'Optional - for use if the PHPgsm database is installed locally';
+	$software['Mysql']['use'] = $ropt.' - for use if the PHPgsm database is installed locally';
 }
 $apache = dpkg('apache2');
 if (isset($apache[2])) {
 $software['Apache']['version'] =  $apache[2];
-$software['Apache']['use'] = 'Optional - '.$apache[4].', only required if using the web API on this machine ';
+$software['Apache']['use'] = $opt.' - '.$apache[4].', only required if using the web API on this machine ';
 }
 else {
 	$software['Apache']['version'] = $apache[1];
-	$software['Apache']['use'] = 'Optional -  only required if using the web API on this machine ';
+	$software['Apache']['use'] = $ropt.' -  only required if using the web API on this machine ';
 }
 if (isset($git[2])) {
 	$software['Git']['version'] = $git[2];
-	$software['Git']['use'] = 'Optional -  '.$git[4].' required to update PHPgsm automatically';
+	$software['Git']['use'] = $opt.' - '.$git[4].' required to update PHPgsm automatically';
 }
 else {
 		$software['Git']['version'] = $git[1];
-		$software['Git']['use'] = 'Optional -  use :- to update PHPgsm automatically';
+		$software['Git']['use'] = $ropt.' -  use :- to update PHPgsm automatically';
 	}
 if (isset($tmpr[2])) {	
 	$software['Tmpreaper']['version'] = $tmpr[2];
-	$software['Tmpreaper']['use'] = 'Optional - '.$tmpr[4].' used for log pruning';
+	$software['Tmpreaper']['use'] = $opt.' - '.$tmpr[4].' used for log pruning';
 }
 else {
 	$software['Tmpreaper']['version'] = $tmpr[1];
-	$software['Tmpreaper']['use'] = 'Optional -  used for log pruning';
+	$software['Tmpreaper']['use'] = $ropt.' -  used for log pruning';
 }	
-if(isset($steam[2])){
-	$software['Steamcmd']['version']  = $steam[2];
-	$software['Steamcmd']['use']  = 'Required - '.$steam[4].' install & update Steam dedicated game servers';
-}
 
-else {
-	$software['Steamcmd']['version']  = $steam[1];
-	$software['Steamcmd']['use']  = 'Required -  use :- install & update Steam dedicated game servers';
-}
-if (isset($glib[2])){
-	$software['GlibC']['version'] = $glib[2];
-	$software['GlibC']['use'] = 'Required - '.$glib[4].' for Steam dedicated game servers';
-}
-else {
-	$software['GlibC']['version'] = $glib[1];
-	$software['GlibC']['use'] = 'Required -  for Steam dedicated game servers';
-}	
-$software['foreign_architecture']['version'] = $x32;
-$software['foreign_architecture']['use'] = 'Required - Steamcmd requires 32bit architecture';
 if(isset($webmin[2])) {
 $software['webmin']['version'] = $webmin[2];
-$software['webmin']['use'] = 'Optional - '.$webmin[4];
+$software['webmin']['use'] = $opt.' - '.$webmin[4];
 }
 else {
 	$software['webmin']['version'] = $webmin[1];
-	$software['webmin']['use'] = 'Optional - web-based administration interface for Unix systems';
+	$software['webmin']['use'] = $ropt.' - web-based administration interface for Unix systems';
 }
 if(isset($loc[2])){
 	$software['locate']['version'] = $loc[2];
-	$software['locate']['use'] = 'Optional - '.$loc[4];
+	$software['locate']['use'] = $opt.' - '.$loc[4];
 }
 else {
 	$software['locate']['version'] = $loc[1];
-	$software['locate']['use'] = 'Optional - quickly find files on the filesystem based on their name';
+	$software['locate']['use'] = $ropt.' - quickly find files on the filesystem based on their name';
 }		
 foreach ($software as $k => $v) {
 	if ($v['version'] !='Not Installed'){ $stat= $tick;} else{$stat = $cross;}
