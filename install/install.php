b84f1a48f78529148e8ba522461033f2d0a92444
Jim <jim@noideersoftware.co.uk>
2021-04-10 17:03:47 +0100

    test file

--- install/install.php
+++ install/install.php
@@ -53,7 +53,8 @@ $x32 = trim(shell_exec('dpkg --print-foreign-architectures'));
 if (empty($x32)) {
 	$x32 = 'Not Enabled';
 }
-$table->addRow(array('Module','   Version' ,'Status',"\t\t\t\tUsage"));
+$table->addRow(array('Module','     Version' ,'Status',"\t\t\t\tUsage"));
+$headmask = "%13.13s";
 $screen = dpkg('screen');
 $loc =dpkg('mlocate');
 $git = dpkg('git'); 
@@ -64,91 +65,92 @@ $webmin = dpkg('webmin');
 $st = dpkg('mysql-server');
 $tmux = dpkg('tmux');
 if(isset($steam[2])){
-	$software['Steamcmd']['version']  = $steam[2];
+	$software['Steamcmd']['version']  = sprintf($headmask,$steam[2]);
 	$software['Steamcmd']['use']  = $req.' - '.$steam[4].' install & update Steam dedicated game servers';
 }
 
 else {
-	$software['Steamcmd']['version']  = $steam[1];
+	$software['Steamcmd']['version']  = sprintf($headmask,$steam[1]);
 	$software['Steamcmd']['use']  = $rreq.' -  use :- install & update Steam dedicated game servers';
 }
 if (isset($glib[2])){
-	$software['GlibC']['version'] = $glib[2];
+	$software['GlibC']['version'] = sprintf($headmask,$glib[2]);
 	$software['GlibC']['use'] = $req.' - '.$glib[4].' for Steam dedicated game servers';
 }
 else {
-	$software['GlibC']['version'] = $glib[1];
+	$software['GlibC']['version'] = sprintf($headmask,$glib[1]);
 	$software['GlibC']['use'] = $rreq.' -  for Steam dedicated game servers';
 }	
-$software['Foreign_Architecture']['version'] = $x32;
+$software['Foreign_Architecture']['version'] = sprintf($headmask,$x32);
 $software['Foreign_Architecture']['use'] = $req.' - Steamcmd requires 32bit architecture';
 if (isset($screen[2])){
-	$software['Screen']['version'] = $screen[2];
+	$software['Screen']['version'] = sprintf($headmask,$screen[2]);
 	$software['Screen']['use'] = $req.' - '.$screen[4].' for Steam dedicated game servers';
 }
 else {
-	$software['Screen']['version'] = $screen[1];
+	$software['Screen']['version'] = sprintf($headmask,$screen[1]);
 	$software['Screen']['use'] = $rreq.' -  for Steam dedicated game servers';
 }	
 if (!isset($st[2])) {
 	$st = dpkg('mysql-common');
 }
 if (isset($st[2])) {
-	$software['Mysql']['version'] = $st[2];
+	
+	$software['Mysql']['version'] = sprintf($headmask,$st[2]);
 	$software['Mysql']['use'] = $opt.' - '.$st[4];
 }
 else {
-	$software['Mysql']['version'] = $st[1];
+	$software['Mysql']['version'] = sprintf($headmask,$st[1]);
 	$software['Mysql']['use'] = $ropt.' - for use if the PHPgsm database is installed locally';
 }
 $apache = dpkg('apache2');
 if (isset($apache[2])) {
-$software['Apache']['version'] =  $apache[2];
-$software['Apache']['use'] = $opt.' - '.$apache[4].', only required if using the web API on this machine ';
+$software['Apache']['version'] =  sprintf($headmask,$apache[2]);
+$software['Apache']['use'] = $opt.' - '.$apache[4].', only required if using the web API on this machine (master server) ';
 }
 else {
-	$software['Apache']['version'] = $apache[1];
+	$software['Apache']['version'] = sprintf($headmask,$apache[1]);
 	$software['Apache']['use'] = $ropt.' -  only required if using the web API on this machine ';
 }
 if (isset($git[2])) {
-	$software['Git']['version'] = $git[2];
+	$software['Git']['version'] = sprintf($headmask,$git[2]);
 	$software['Git']['use'] = $opt.' - '.$git[4].' required to update PHPgsm automatically';
 }
 else {
-		$software['Git']['version'] = $git[1];
+		$software['Git']['version'] = sprintf($headmask,$git[1]);
 		$software['Git']['use'] = $ropt.' -  use :- to update PHPgsm automatically';
 	}
 if (isset($tmpr[2])) {	
-	$software['Tmpreaper']['version'] = $tmpr[2];
+	$software['Tmpreaper']['version'] = sprintf($headmask,$tmpr[2]);
 	$software['Tmpreaper']['use'] = $opt.' - '.$tmpr[4].' used for log pruning';
 }
 else {
-	$software['Tmpreaper']['version'] = $tmpr[1];
+	$software['Tmpreaper']['version'] = sprintf($headmask,$tmpr[1]);
 	$software['Tmpreaper']['use'] = $ropt.' -  used for log pruning';
 }	
 if (isset($tmux[2])) {	
-	$software['Tmux']['version'] = $tmux[2];
+	$software['Tmux']['version'] = sprintf($headmask,$tmux[2]);
 	$software['Tmux']['use'] = $opt.' - '.$tmux[4].' used for LGSM compatability';
 }
 else {
-	$software['Tmux']['version'] = $tmux[1];
+	$software['Tmux']['version'] = sprintf($headmask,$tmux[1]);
 	$software['Tmux']['use'] = $ropt.' -  terminal multiplexer used for LGSM compatability';
 }	
 if(isset($webmin[2])) {
-$software['Webmin']['version'] = $webmin[2];
+$software['Webmin']['version'] = sprintf($headmask,$webmin[2]);
 $software['Webmin']['use'] = $opt.' - '.$webmin[4];
 }
 else {
 	$err = trim($cc->convert("%r".$webmin[1]."%n"));
-	$software['Webmin']['version'] =$webmin[1]; //
+	$software['Webmin']['version'] = sprintf($headmask,$webmin[1]); //
 	$software['Webmin']['use'] = $ropt.' - web-based administration interface for Unix systems';
 }
 if(isset($loc[2])){
-	$software['Locate']['version'] = $loc[2];
+	$software['Locate']['version'] = sprintf($headmask,$loc[2]);
 	$software['Locate']['use'] = $opt.' - '.$loc[4];
 }
 else {
-	$software['Locate']['version'] = $loc[1];
+	$software['Locate']['version'] = sprintf($headmask,$loc[1]);
 	$software['Locate']['use'] = $ropt.' - quickly find files on the filesystem based on their name';
 }		
 foreach ($software as $k => $v) {
@@ -158,30 +160,30 @@ foreach ($software as $k => $v) {
 }
 unset($software);
 $php_v = PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;
-$software['php']['version'] = phpversion();
+$software['php']['version'] = sprintf($headmask,phpversion());
 $software['php']['use'] = "$req - server-side, HTML-embedded scripting language";
 $pmysql = dpkg('php'.$php_v.'-mysql');
-$software['php_mysql']['version'] = $pmysql[2];
+$software['php_mysql']['version'] = sprintf($headmask,$pmysql[2]);
 $software['php_mysql']['use'] ="$req - ".$pmysql[4];
-$software['php_gmp']['version'] = phpversion('gmp');
+$software['php_gmp']['version'] = sprintf($headmask,phpversion('gmp'));
 $software['php_gmp']['use'] ="$req - ".'GMP module for PHP - provides advanced math functions';
 if (!empty(phpversion('zip'))) {
-$software['php_zip']['version'] = phpversion('zip');
+$software['php_zip']['version'] = sprintf($headmask,phpversion('zip'));
 }
 else {
-	$software['php_zip']['version'] = 'Not Installed';
+	$software['php_zip']['version'] = sprintf($headmask,'Not Installed');
 }
 $software['php_zip']['use'] ="$opt - ZIP module for PHP - provides archive functions required for later versions of PHPgsm";
-$software['php_xml']['version'] = phpversion('xml');
+$software['php_xml']['version'] = sprintf($headmask,phpversion('xml'));
 $software['php_xml']['use'] ="$req - ".'XML module for PHP - provides xml data support';
-$software['php_json']['version'] = phpVersion('json'); // virtual pack as of 8.0 let php work it out
+$software['php_json']['version'] = sprintf($headmask,phpVersion('json')); // virtual pack as of 8.0 let php work it out
 $software['php_json']['use'] ="$req - JSON module for PHP";
-$software['php_mbstring']['version'] = phpversion('mbstring');
+$software['php_mbstring']['version'] = sprintf($headmask,phpversion('mbstring'));
 $software['php_mbstring']['use'] ="$req - MBSTRING module for PHP - provides database functions for multibyte objects";
-$software['php_readline']['version'] = phpversion('readline');
+$software['php_readline']['version'] = sprintf($headmask,phpversion('readline'));
 $software['php_readline']['use'] ="$req - READLINE module for PHP ";
 $popcache = dpkg('php'.$php_v.'-opcache');
-$software['php_opcache']['version'] = $popcache[2];
+$software['php_opcache']['version'] = sprintf($headmask,$popcache[2]);
 $software['php_opcache']['use'] = "$opt - ".$popcache[4];
 //print_r($software);
 //die();
@@ -195,9 +197,9 @@ foreach ($software as $k => $v) {
 unset($software);
 $table->addRow(array('','' ,'',''));
 $table->addRow(array($cc->convert("%yPHPgsm Modules%n"),'' ,''));
-$software['Ajax'] = getVersion('php ../ajaxv2.php action=version');
-$software['Scanlog'] = getVersion('../scanlog.php v');
-$software['Functions'] = getVersion('php ../functions.php -v');
+$software['Ajax'] = sprintf($headmask,getVersion('php ../ajaxv2.php action=version'));
+$software['Scanlog'] = sprintf($headmask,getVersion('../scanlog.php v'));
+$software['Functions'] = sprintf($headmask,getVersion('php ../functions.php -v'));
 foreach ($software as $k => $v) {
 	if ($v !=''){ $stat= $tick;} else{$stat = $cross;}
 	$k = str_replace('_','-',$k);
