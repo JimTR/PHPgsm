561c0280da21c60d11ea50e83cfcccff52747b9a
JimTR <jim.richardson06@btinternet.com>
2021-04-12 08:40:25 +0100

    bug fix

--- cron_u.php
+++ cron_u.php
@@ -59,7 +59,9 @@ $sql = 'SELECT servers.* , base_servers.url, base_servers.port FROM `servers` le
 	
 	foreach ($res as $data) {
 		        $acf_loc = $data['location'].'/steamapps/appmanifest_'.$data['server_id'].'.acf';
-				$disk_size = shell_exec('du -hs '.$data['location']);	    
+				$disk_size = trim(shell_exec('du -hs '.$data['location']));
+				$ds = substr($disk_size,0,strpos($disk_size,'/'));
+	    
 			    $local =  check_local($acf_loc);
 			    
 			    
@@ -140,7 +142,7 @@ $sql = 'SELECT servers.* , base_servers.url, base_servers.port FROM `servers` le
 		unset($update);
 		unset($where);
 		$where['host_name'] = $data['host_name'];
-		$update['disk_space'] =$disk_size;
+		$update['disk_space'] = $ds;
 		$database->update('servers',$update,$where);
 	}
 		function local_update($build,$local) {
