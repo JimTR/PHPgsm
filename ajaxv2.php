4b2945900a07bb31062c07a2d0907d5e1a80b006
Jim <jim@noideersoftware.co.uk>
2021-05-07 07:46:31 +0100

    change unknown flag

--- ajaxv2.php
+++ ajaxv2.php
@@ -727,7 +727,7 @@ if (count($players)) {
 			// add a default image for the flag
 			// random country
 			if(empty(trim($players[$k]['Name']))) { $players[$k]['Name'] = 'Setting Up';}
-			$players[$k]['flag'] = 'src ="img/'.'unknown.png"'; // windows don't do emoji flags use image
+			//$players[$k]['flag'] = 'src ="img/'.'unknown.png"'; // windows don't do emoji flags use image
 			$players[$k]['country'] = 'undesclosed';
 			
 		}
