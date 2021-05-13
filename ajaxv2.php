7eb90a0dcbc570bf72d015b737e802187f897995
Jim <jim@noideersoftware.co.uk>
2021-05-13 08:43:05 +0100

    logold busted in production ?

--- ajaxv2.php
+++ ajaxv2.php
@@ -882,7 +882,7 @@ function scanlog($cmds) {
 			
 			if (isset($cmds['debug']) && $cmds['debug'] == 'true') {
 				echo print_r($logold,true).cr;
-				echo 'file changed records => '.$current_records-$lastrecord.'/'.$current_records.cr;
+				echo 'file changed records => '.intval($current_records)-$lastrecord.'/'.$current_records.cr;
 			}
 			
 		unset($return);
