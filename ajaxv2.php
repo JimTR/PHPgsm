82fe108763e40384acc35cf9b10d6387a9bacf4f
Jim <jim@noideersoftware.co.uk>
2021-05-23 11:43:56 +0100

    rework game_detail

--- ajaxv2.php
+++ ajaxv2.php
@@ -414,8 +414,8 @@ function game_detail() {
 												//$server1= trim($server1); // get rid of spaces & CR's 
 												//$tmp_array[$i] = explode(' ',$server1); // arrayify
 												// temp log
-												echo 'ps -a -o pid,cmd |grep "'.$server['startcmd'].'" |grep -v grep'.cr;
-												exec('ps -a -o pid,cmd |grep "'.$server['startcmd'].'" |grep -v grep',$server_ps,$ret);
+												echo 'ps a -o pid,cmd |grep "'.$server['startcmd'].'" |grep -v grep'.cr;
+												exec('ps a -o pid,cmd |grep "'.$server['startcmd'].'" |grep -v grep',$server_ps,$ret);
 												echo print_r ($server_ps,true).cr;
 												$detail= explode(' ',$server_ps[0]);
 												echo print_r($detail,true).cr;
