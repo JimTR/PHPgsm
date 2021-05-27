fd9187c58ea22947cdbf4da6f1d21983da5a7219
Jim <jim@noideersoftware.co.uk>
2021-05-27 08:06:25 +0100

    update

--- ajaxv2.php
+++ ajaxv2.php
@@ -37,9 +37,9 @@ error_reporting (0);
 $update_done= array();
 $ip = $_SERVER['SERVER_ADDR']; // get calling IP
 $sql = 'select * from base_servers where base_servers.ip ="'.$_SERVER['REMOTE_ADDR'].'"'; // do we know this ip ? mybb sets this at login
-echo $sql.'<br>';
+//echo $sql.'<br>';
 $valid = $database->num_rows($sql); // get result if the ip can use the data the return value >0
-echo $ip." Valid = $valid <br>";
+//echo $ip." Valid = $valid <br>";
 if(is_cli()) {
 	$valid = 1; // we trust the console
 	$sec = true;
