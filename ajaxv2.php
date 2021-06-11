09095e5af4d977e91144d6ab4cca73b6374e78fe
Jim <jim@noideersoftware.co.uk>
2021-06-11 17:59:40 +0100

    fix time stutter to login time

--- ajaxv2.php
+++ ajaxv2.php
@@ -1161,7 +1161,7 @@ foreach ($la as $user_data) {
 		if ($last_logon >  $result['last_log_on']) {
 			$result['last_log_on'] = $last_logon;
 			$result['log_ons'] ++;
-			$ut.= ' new logon at '.date('hh:mm:ss',$last_logon).' (total '.$result['log_ons'].')';
+			$ut.= ' new logon at '.date('h:m:s',$last_logon).' (total '.$result['log_ons'].')';
 			$modify=true;
 			$logon = true;
 		}
