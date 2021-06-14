d6d04c4a5608c957676061f8beefbba3ddb777a5
Jim <jim@noideersoftware.co.uk>
2021-06-14 07:55:48 +0100

    added time to output

--- scanlog.php
+++ scanlog.php
@@ -246,7 +246,7 @@ foreach ($la as $user_data) {
 		if ($last_logon >  $result['last_log_on']) {
 			$result['last_log_on'] = $last_logon;
 			$result['log_ons'] ++;
-			$ut.= ' new logon (total '.$result['log_ons'].')';
+			$ut.= ' new logon  at '.date('h:m:s',$last_logon).' (total '.$result['log_ons'].')';
 			$modify=true;
 			$logon = true;
 		}
