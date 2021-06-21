62e76995a48a135a09862dc120d8e78be4056bd0
Jim <jim@noideersoftware.co.uk>
2021-06-21 11:18:29 +0100

    add build

--- includes/class.color.php
+++ includes/class.color.php
@@ -46,6 +46,7 @@
  * @license  http://www.opensource.org/licenses/mit-license.php MIT License
  * @link     http://pear.php.net/package/Console_Color
  */
+	define ('BUILD',"10509-348196329");
 class Console_Color2
 {
 
