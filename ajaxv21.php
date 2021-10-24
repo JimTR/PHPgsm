191e2bd6110059ada6a457ea59e84a1cae991104
Jim <jim@noideersoftware.co.uk>
2021-10-24 11:08:47 +0100

    does ascii work ?

--- ajaxv21.php
+++ ajaxv21.php
@@ -29,34 +29,59 @@ require DOC_ROOT. '/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 	define( 'SQ_ENGINE',      SourceQuery::SOURCE );
 	define( 'LOG',	'logs/ajax.log');
 	define ('CR',PHP_EOL);
-	$build = "46611-3909981191";
-	$version = 2.10;
-print_r($argv);
+	$build = "4657-1166691063";
+	$version = 2.101;
 	$cmds =startup();
+	//echo 'returned $cmds ',print_r($cmds,true).cr;
         if($cmds['action'] == 'version'){
            echo 'Ajax v'.$version.' '.$build.' Copyright Noideer Software '.$settings['start_year'].' - '.date('Y').cr;
         }
-echo 'running';
+        if($cmds['action'] =='help') {die(help());}
+echo 'running'.cr;
 	function startup() {
 		// get supplied options
-                global $argv; 
+                
                 $output ='';
+               
 		if (is_cli()) {
-                        echo 'in cli'.CR;
-			$shortopts ="a:s:";
+			global $argv; 
+			 //echo 'raw argv '.print_r($argv,true);
+                $argv = array_map('strtolower',$argv);
+               // echo 'lowered argv '.print_r($argv,true);
+            //echo 'in cli'.CR;
+			$shortopts ="a:A:s:S:d::D::v::V::h::H::";
 			$longopts[] = "debug::";
+			$longopts[] = "DEBUG::";
 			$longopts[] = "action:";
 			$longopts[] = "server:";
-			$cmds = getopt($shortopts,$longopts);
+			$longopts[] = "version";
+			$longopts[] = "VERSION";
+			$longopts[] = "help::";
+			$longopts[] = "HELP::";
+			//$result = array_map('strtolower',$myArray);
+			$options = getopt($shortopts,$longopts);
+			//echo 'options as is  '.print_r($options,true);
+			$options = array_change_key_case($options,CASE_LOWER);
+			$options = array_map('strtolower',$options);
+			//echo 'case changed '.print_r($options,true);
 			// running from the command line
+			//echo 'options '.print_r($options,true).CR;
 			$cmds['valid'] = 1; // we trust the console
 			$method = 'cli';
 			define ('cr',PHP_EOL);
-                        $cmds = convert_to_argv($argv,"",true);
-                         
+			if (!isset($argv['action'])) { 
+				$cmds = convert_to_argv($argv,"",true);
+			}
+			if(isset($options['debug'])) {$cmds['debug']= true;}
+            if(isset($options['v']) or isset($options['version'])) {$cmds['action'] ='version';}
+            if(isset($options['h']) or isset($options['help'])) {$cmds['action'] ='help';}
+            if(isset($options['a'])) {$cmds['action'] = $options['a'];} 
+            //switch ($options) {
+				
+           
                         //die('cli');
 		}
-//echo 'cli done<br>';
+
 		//else {
 			// run via url
                         //echo 'run by url<br>';
@@ -104,8 +129,29 @@ echo 'running';
                if (isset($_SERVER['HTTP_PHPGSM_AUTH'])) {
                         $cmds['valid'] = 1;
                         //echo 'auth on'.cr;
-                        //echo $output;
+                        
                 }
+                //echo $output;
             return $cmds; 
+	}
+	function help($option=null) {
+		// display help
+		global $version,$build,$settings;
+		$year = $settings['start_year'];
+		$date = date('Y');
+		$cc = new Color();
+		echo $cc->convert("%GAjax v$version $build Copyright Noideer Software $year - $date%n").cr;
+		$table = new Table(CONSOLE_TABLE_ALIGN_LEFT, array('horizontal' => '', 'vertical' => '', 'intersection' => ''));
+		$option = $cc->convert("%cOption\t\t\t%n");
+	    $use = $cc->convert("%cUse\t\t\t%n");
+	    $notes = $cc->convert("%c\tNotes%n");
+	    echo cr;
+	    $table->setHeaders( array ($option,$use,$notes));
+		//$table->addRow(array('','',''));
+		//$table->addRow(array($option,$use,$notes));
+		$table->addRow(array('-h, --help' ,'get help','display help on a subject e.g \'--help action\''));
+		$table->addRow(array('-a, --action' ,'send action','major option must be set'));
+		$table->setHeaders( array ($option,$use,$notes));
+		echo $table->getTable().cr;
 	}
 ?>
