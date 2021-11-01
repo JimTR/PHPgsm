7c7b3c15852562fa040f24c53f85921560b22b34
Jim <jim@noideersoftware.co.uk>
2021-11-01 10:34:44 +0000

    xml header

--- ajaxv21.php
+++ ajaxv21.php
@@ -37,7 +37,7 @@ require DOC_ROOT. '/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 	define ('borders',array('horizontal' => '─', 'vertical' => '│', 'intersection' => '┼','left' =>'├','right' => '┤','left_top' => '┌','right_top'=>'┐','left_bottom'=>'└','right_bottom'=>'┘','top_intersection'=>'┬'));
 	define ('no_borders',array('horizontal' => '', 'vertical' => '', 'intersection' => '','left' =>'','right' => '','left_top' => '','right_top'=>'','left_bottom'=>'','right_bottom'=>'','top_intersection'=>''));
 	define ('IN_PHPGSM','');
-	$build = "10443-689024457";
+	$build = "10498-3590515269";
 	$version = 2.101;
 	$cmds = startup();
 	//print_r($argv);
@@ -95,6 +95,7 @@ require DOC_ROOT. '/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 					echo json_encode($content);
 					break;
 				case 'xml':
+						header('Content-Type: text/xml; charset=UTF-8');
 						echo  arrayToXML($content, new SimpleXMLElement('<scanlog/>'), 'output');
 						break;
 				case 'text':	
