a4234a9eaa91b66a1f8306c91fd117072827fd0f
Jim <jim@noideersoftware.co.uk>
2021-11-05 09:17:27 +0000

    start work on readlog to go with auto ban

--- ajaxv2.php
+++ ajaxv2.php
@@ -31,7 +31,7 @@ require DOC_ROOT. '/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 	define( 'LOG',	'logs/ajax.log');
 	define ('cr',PHP_EOL);
 	define ('CR',PHP_EOL);
-	$build = "48959-3257276800";
+	$build = "48960-933891121";
 	$version = 2.07;
 error_reporting (0);
 $update_done= array();
@@ -924,7 +924,7 @@ function readlog($cmds) {
 				}
 			if (strpos($v,'permanent') or strpos($v,' min')) {
 				// do ban lines
-				$data = explode($v,':');
+				$data = explode(':',$v);
 				$v = print_r($data,true);
 			}	
 			$v = str_replace('"','',$v); // knock out "
