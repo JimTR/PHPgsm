702630d420bee5bf5a9d96b7e336b968c5c9a0de
Jim <jim@noideersoftware.co.uk>
2021-11-05 08:48:34 +0000

    start work on readlog to go with auto ban

--- ajaxv2.php
+++ ajaxv2.php
@@ -31,7 +31,7 @@ require DOC_ROOT. '/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 	define( 'LOG',	'logs/ajax.log');
 	define ('cr',PHP_EOL);
 	define ('CR',PHP_EOL);
-	$build = "48636-3196678353";
+	$build = "48716-3792349175";
 	$version = 2.07;
 error_reporting (0);
 $update_done= array();
@@ -918,9 +918,14 @@ function readlog($cmds) {
 			$v = str_replace('Writing ','<span style="color:green"><b>Writing </b></span>',$v);
 			$v = str_replace('Unknown command','<span style="color:red"><b>Unknown command </b></span>',$v);
 			$v = str_replace('listip','<span style="color:blue"><b>List Banned Ip\'s </b></span>',$v);
+			if (strpos($v,'IP filter list')) {
+				$v ='';
+				}
 			$v = str_replace('"','',$v); // knock out "
 		}
-		$return[] = $v;
+		if(!empty($v)) {
+			$return[] = $v;
+		}
 	}
 	return array_reverse($return);	// send the data back in the right order !
 }
