6ce0fec174c29ba49cf44455635903d5a9355cb9
Jim <jim@noideersoftware.co.uk>
2021-11-08 09:46:31 +0000

    multiple IP addresses in exe function

--- ajaxv2.php
+++ ajaxv2.php
@@ -32,7 +32,7 @@ require DOC_ROOT. '/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 	define ('cr',PHP_EOL);
 	define ('CR',PHP_EOL);
 	define ('borders',array('horizontal' => '─', 'vertical' => '│', 'intersection' => '┼','left' =>'├','right' => '┤','left_top' => '┌','right_top'=>'┐','left_bottom'=>'└','right_bottom'=>'┘','top_intersection'=>'┬'));
-	$build = "50481-334292200";
+	$build = "50482-2864450167";
 	$version = 2.07;
 error_reporting (0);
 $update_done= array();
@@ -399,7 +399,7 @@ function game_detail() {
 					$ip = gethostbyname($host);
 					$localIP = trim(shell_exec('hostname -I'));
 	                $localIPs = explode(' ',$localIP);
-	                print_r($localIPs);
+	                //print_r($localIPs);
 					//$ip = geturl('https://api.ipify.org');
 					if (empty($ip)) { $ip = geturl('http://ipecho.net/plain');}
 				}
