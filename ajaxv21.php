c1c8ab25221ee64df8c565e9afc51f635f3d1c1d
Jim <jim@noideersoftware.co.uk>
2021-11-12 08:45:31 +0000

    console code

--- ajaxv21.php
+++ ajaxv21.php
@@ -120,9 +120,9 @@ require DOC_ROOT. '/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 			    echo 'console code<body style="background:#000;color:#ccc;">'.cr;
 			    echo 'returned $cmds '.cr,printr($cmds).cr;
 			    $output = readlog($cmds,$file);
-			    if(!isset($cmds['colour']) or $cmds['colour'] == false) {
-					$output = strip_tags($output,'<br> <table> <tr> <td>');
-				}
+			    //if(!isset($cmds['colour']) or $cmds['colour'] == false) {
+					//$output = strip_tags($output,'<br> <table> <tr> <td>');
+				//}
 
 				foreach ($output as $show) {
 					echo $show;
