37199dd4c1b6a7c916f6f9326be3da202d64c9b2
JimTR <jim.richardson06@btinternet.com>
2021-11-17 17:20:35 +0000

    console code

--- modules/console.php
+++ modules/console.php
@@ -1,19 +1,23 @@
 <?php
 //$cmds = $argv;
 include 'console_colour.php';
+include 'colours/'.$cmds['game'].'-console_colour.php';
+//print_r($cmds);
 function readlog($cmds,$file='') {
-global $console;
+global $console, $settings;
 $roll_back = $cmds['rows'];
 $count=0;
 $a = file($file);
 
-if (empty($n)) {
-      $n = count($a)-2;
+if (empty($roll_back)) {
+      $roll_back = count($a)-2;
 }
 $arr = array_slice($a, - $roll_back);
 $stats = false;
 foreach ($arr as  $v) {
         if($v[0] == '#') {continue;}
+        if($v[0] == '*') {continue;}
+        
 		$v = rtrim($v); 
 	    preg_match('/\d+\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+/', $v, $result);
         if (isset($result[0])){$ips = $result[0];} else{$ips = '';}
@@ -23,7 +27,16 @@ foreach ($arr as  $v) {
          if(isset($result[1])){$steam_id = $result[1];} else {$steam_id='';}
         unset($result);
         preg_match('/(\d+)\/(\d+)\/(\d+) - (\d+):(\d+):(\d+)/', $v, $result);
-        if(isset($result[0])){$date_time = $result[0];} else{$date_time = '';}
+        if(isset($result[0])){
+			$date_time = explode('-', $result[0]);
+			// format date time as per settings
+			$date_time[0] = date($settings['date_format'],strtotime($date_time[0]));
+			//echo "hello ".print_r($date_time,true) ;
+			$date_time = implode(' -',$date_time);
+			} 
+			else{
+				$date_time='' ;
+				}
         unset($result);
         $date_time = preg_replace('/(\d+)\/(\d+)\/(\d+) - (\d+):(\d+):(\d+)/', '<span style="color:yellow">$0</span>', $date_time);
         $v = preg_replace('/L (\d+)\/(\d+)\/(\d+) - (\d+):(\d+):(\d+)/','',$v);
@@ -69,8 +82,8 @@ foreach ($arr as  $v) {
                                 //echo $type.PHP_EOL;
                                 $count=0;
                                 if($count == 0){
-                                   if($type =='IP') {$v = "<table style='width:20%;'><tr><td style='width:50%;text-align:center;'>IP Address</td><td>Ban Period</td></tr>";}
-                                   else {$v = "<table style='width:20%;'><tr><td style='width:50%;text-align:center;'>User ID</td><td>Ban Period</td></tr>";}     
+                                   if($type =='IP') {$v = "<table style='width:30%;'><tr><td style='width:50%;text-align:center;'>IP Address</td><td>Ban Period</td></tr>";}
+                                   else {$v = "<table style='width:30%;'><tr><td style='width:50%;text-align:center;'>User ID</td><td>Ban Period</td></tr>";}     
                                  } 
 				
 				$count = intval($data[1]);
@@ -103,7 +116,7 @@ foreach ($arr as  $v) {
                                         unset($uobject[5]);
                                    }
 
-				$v = '<tr><td style="text-align:right;padding-right:11%;">'.trim($uobject[1]).'</td><td>'.trim($data[1]).'</td></tr>' ;
+				$v = '<tr><td style="text-align:right;padding-right:11%;color:cyan;">'.trim($uobject[1]).'</td><td style="color:red">'.trim($data[1]).'</td></tr>' ;
 			}
                                 if($uobject[0] == $count) {
                                    //echo 'we hit the full amount'.PHP_EOL;
@@ -115,7 +128,7 @@ foreach ($arr as  $v) {
                                $data = explode(' ',$v);
                                //echo print_r($data,true); 
                                $v = $data[3].' - '.$data[1];
-                               $v = '<tr><td style="text-align:right;padding-right:11%;">'.trim($data[1]).'</td><td>'.trim($data[3]).'</td></tr>' ;
+                               $v = '<tr><td style="text-align:right;padding-right:11%;color:cyan;">'.trim($data[1]).'</td><td style="color:red">'.trim($data[3]).'</td></tr>' ;
                                 //$v="count = $count ".print_r($data,true);
                                  if($data[0] == $count) {
                                    //echo 'we hit the full amount'.PHP_EOL;
@@ -130,10 +143,18 @@ foreach ($arr as  $v) {
 			if (strpos($v,'Addip:')) {
                            $v = trim(str_replace('"','',str_replace(')','',$v)));
                            $data = explode(' ',$v);
-                           //print_r($data);
+                           $data_count = count($data)-1;
                            $ip = trim(str_replace(')','',end($data)));
                             //echo "ip = $ip".'<br>';
-                           $v =$data[1].' '.$data[6]." $ip ".' '.$data[3].' '.$data[7].'  '.$data[4].' '.$data[5].' '.$data[9];
+                            if($data_count == 13) {
+								// add line
+								//$v = printr($data);
+								
+								$v = $data[1].' '.$data[6]." $ip ".$data[3].' '.$data[4].' '.$data[7].' '.intval($data[8]).' '.$data[9].' '.$data[10].' '.$data[11];
+							}
+							else{
+								$v =$data[1].' '.$data[6]." $ip ".' '.$data[3].' '.$data[7].'  '.$data[4].' '.$data[5].' '.$data[9];
+							}
                            //$v = print_r($data,true); 
                            //addip
 			}
@@ -154,24 +175,50 @@ foreach ($arr as  $v) {
          $v = preg_replace('/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}/', '$1 <span style="color:'.$console['user_ip'].';">$0</span>', $v);
          $v = preg_replace('/sv_[^=]*/', '$1<span style="color:cyan;">$0</span>', $v);
          $v = preg_replace('/tv_[^=]*/', '$1<span id="bug" style="color:cyan;">$0</span>', $v); 
+         $v = preg_replace('/tf_[^=]*/', '$1<span id="bug" style="color:cyan;">$0</span>', $v);
+         $v = preg_replace('/mp_[^=]*/', '$1<span id="bug" style="color:cyan;">$0</span>', $v);  
          $v = preg_replace('/(\w+\/){1,5}\w+\.\w+/', '<span style="color:Violet;">$0</span>', $v);
-     
+         if(function_exists('str_starts_with')) {
+			if (str_starts_with($v,'Player')){continue;};
+			if (str_starts_with($v,'userid')){continue;};
+			if (str_starts_with($v,')')){continue;};
+			if (str_starts_with($v, 'Client')) {continue;}
+			if (str_starts_with($v, 'Dropped')) {continue;}
+			if (str_starts_with($v, 'Weapon')) {continue;}
+			if (str_starts_with($v, 'name')) {continue;}
+			if (str_starts_with($v, 'Ignoring')) {continue;}
+		}
+		else {
+			if(startsWith($v,'Player')){continue;}
+			if (startsWith($v, 'Dropped')) {continue;}
+		}
+        // if(strlen($user) >1) {
+			// echo strlen($user).' '.$v.'<br>';
+         //if (str_starts_with($v, $user)) {continue;}
+	 //}
        if (!strpos($v,'</tr>')){
          if(isset($cmds['colour']) and $cmds['colour'] == false) {
-                $date_time = strip_tags($date_time);
-		$v = strip_tags($v,'<br> <table> <tr> <td>');
+             $date_time = strip_tags($date_time);
+		     $v = strip_tags($v,'<br> <table> <tr> <td>');
 		} 
-         $return[] = "<div>$date_time $user $v</div>"; // build line here
+		  $v = substr_replace($v, $user, 2, 0); 
+         $return[] = "<div>$date_time $v</div>"; // build line here
       
     	}
         else {
-              if(!isset($cmds['colour']) or $cmds['colour'] == false) {
+           if(isset($cmds['colour']) or $cmds['colour'] == false) {
                 $v = strip_tags($v,'<br> <table> <tr> <td>');
-                } 
+           } 
 
-          $return[] = $v;
+          $return[] = "<div>$v</div>";
           }
 	}
 }
 return $return;
+}
+
+function startsWith($haystack, $needle)
+{
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
 }
