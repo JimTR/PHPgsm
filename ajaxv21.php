5e81f5bbf68754003932aeffe3c07dc200a56480
Jim <jim@noideersoftware.co.uk>
2021-11-19 15:53:42 +0000

    start a bit of error handling

--- ajaxv21.php
+++ ajaxv21.php
@@ -37,7 +37,7 @@ require DOC_ROOT. '/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 	define ('borders',array('horizontal' => '─', 'vertical' => '│', 'intersection' => '┼','left' =>'├','right' => '┤','left_top' => '┌','right_top'=>'┐','left_bottom'=>'└','right_bottom'=>'┘','top_intersection'=>'┬'));
 	define ('no_borders',array('horizontal' => '', 'vertical' => '', 'intersection' => '','left' =>'','right' => '','left_top' => '','right_top'=>'','left_bottom'=>'','right_bottom'=>'','top_intersection'=>''));
 	define ('IN_PHPGSM','');
-	$build = "12894-3877486117";
+	$build = "12897-1040397073";
 	$version = 2.101;
 	$cmds = startup();
 	//print_r($argv);
@@ -317,7 +317,7 @@ function output ($content, $type,$node,$sub_node) {
 	// do output
 	switch ($type) {
 		case "json":
-			if ($node == 'error') {
+			if ($node == '<error/>') {
 				$error[$node]=$content;
 				$content = $error; 
 			}
