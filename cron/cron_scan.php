e45c9c07d2ff852ff938cf6795d5314bd3e0fed9
Jim <jim@noideersoftware.co.uk>
2021-11-28 13:46:41 +0000

    got the variables the wrong way around

--- cron/cron_scan.php
+++ cron/cron_scan.php
@@ -31,7 +31,7 @@ require DOC_ROOT.'/includes/class.emoji.php';
 require DOC_ROOT.'/includes/class.steamid.php';
 $version = 1.01;
 define("VERSION",$version);
-	$build = "16551-1149737173"
+	$build = "16551-1149737173";
     $shortopts ="i:s:v::";
 	$longopts[]="debug::";
 	$longopts[]="help::";
