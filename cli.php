3ecb298a6b75056a9865eb2bd065a0f4093bb51b
Jim <jim@noideersoftware.co.uk>
2021-12-10 15:17:01 +0000

    you guessed forgot to build the file

--- cli.php
+++ cli.php
@@ -58,9 +58,9 @@ require DOC_ROOT. '/inc/xpaw/SourceQuery/bootstrap.php'; // load xpaw
 	define( 'SQ_ENGINE',      SourceQuery::SOURCE );
 	define( 'LOG',	'logs/ajax.log');
 
-$build = "30311-1205682581";
+$build = "30312-2811487679";
 $version = "3.00";
-$time = "1639148864";
+$time = "1639149380";
 	define ('cr',PHP_EOL);
 	define ('CR',PHP_EOL);
 	define ('borders',array('horizontal' => '─', 'vertical' => '│', 'intersection' => '┼','left' =>'├','right' => '┤','left_top' => '┌','right_top'=>'┐','left_bottom'=>'└','right_bottom'=>'┘','top_intersection'=>'┬'));
