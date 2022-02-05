b00d0d820b98e424b2c11ba2b37f4e27f9f7162e
Jim <jim@noideersoftware.co.uk>
2022-02-05 09:55:21 +0000

    add some extras

--- includes/allowed_cmds.php
+++ includes/allowed_cmds.php
@@ -3,10 +3,14 @@
 * add system commands you want PHPgsm to be allowed to run
 * supply full paths if you wish (required if you are running something with elevated privs) 
 */
+$module = "allowed commands";
 $version = "1.00";
-$build = "312-1936565686";
-$allowed[]='steamcmd';
-$allowed[]='tmpreaper';
-$allowed[]='scanlog.php';
-$allowed[]='cron_u.php';
-$allowed[] ='check_ud.php';
+$time = "1644054921";
+$allowed[] = 'steamcmd';
+$build = "381-3744133786";
+$allowed[] = 'tmpreaper';
+$allowed[] = 'scanlog.php';
+$allowed[] = 'cron_u.php';
+$allowed[] = 'check_ud.php';
+$allowed[] = 'sudo';
+$allowed[] = 'cron_scan.php';
